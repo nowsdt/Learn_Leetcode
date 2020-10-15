399e864ad7635168d7968ba3b66c62042233b758
xnshidaituan <shidaituan@imdada.cn>
2020-10-15 14:56:02 +0800

    1.add guava 2.fiber

--- src/cn/supertao/hot100/Solution003.java
+++ src/cn/supertao/hot100/Solution003.java
@@ -1,8 +1,10 @@
 package cn.supertao.hot100;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -32,19 +34,41 @@ import java.util.List;
  */
 public class Solution003 {
     public static void main(String[] args) {
-        final List<String> strList = Arrays.asList("abcabcbb"/*, "bbbbb", "pwwkew"*/);
+        final List<String> strList = Arrays.asList("abcabcbb", "bbbbb", "pwwkew");
         for (String str : strList) {
             final long start = System.nanoTime();
             final Integer length = lengthOfLongestSubstring(str);
             System.out.println("耗时："+ (System.nanoTime() - start)  + "ns");
             System.out.println(length);
         }
-        System.out.println("==============");
+        System.out.println("======滑动窗口========");
         for (String str : strList) {
             final long start = System.nanoTime();
             final Integer length = lengthOfLongestSubstringSmoothWin(str);
             System.out.println("耗时："+ (System.nanoTime() - start)  + "ns");
             System.out.println(length);
+        }
+        System.out.println("=======滑动窗口优化0=======");
+        for (String str : strList) {
+            final long start = System.nanoTime();
+            final Integer length = lengthOfLongestSubstringSmoothWinOpt0(str);
+            System.out.println("耗时："+ (System.nanoTime() - start)  + "ns");
+            System.out.println(length);
+        }
+        System.out.println("=======滑动窗口优化1=======");
+        for (String str : strList) {
+            final long start = System.nanoTime();
+            final Integer length = lengthOfLongestSubstringSmoothWinOpt1(str);
+            System.out.println("耗时："+ (System.nanoTime() - start)  + "ns");
+            System.out.println("结果：" + length);
+        }
+
+        System.out.println("=======滑动窗口优化2=======");
+        for (String str : strList) {
+            final long start = System.nanoTime();
+            final Integer length = lengthOfLongestSubstringSmoothWinOpt2(str);
+            System.out.println("耗时："+ (System.nanoTime() - start)  + "ns");
+            System.out.println("结果：" + length);
         }
 
     }
@@ -55,11 +79,94 @@ public class Solution003 {
      * @param str
      * @return
      */
-    public static Integer lengthOfLongestSubstringSmoothWinOpt(String str) {
+    public static Integer lengthOfLongestSubstringSmoothWinOpt2(String str) {
+        int max = 0;
+        final int length;
+        if (str == null || (length = str.length()) == 0) {
+            return 0;
+        }
+
+        //Map<char, index>
+        final Map<Character, Integer> container = new HashMap();
+
+        int i = 0;
+
+        for (int j = 0; j < length; j++) {
+            final char charAt = str.charAt(j);
+
+            if (container.containsKey(charAt)) {
+                i = Math.max(i, container.get(charAt));
+            }
+
+            container.put(charAt, j + 1);
+
+            max = Math.max(max, j -i +1);
+
+        }
+        return max;
+    }
+    /**
+     * 优化的滑动窗口算法
+     *
+     * @param str
+     * @return
+     */
+    public static Integer lengthOfLongestSubstringSmoothWinOpt1(String str) {
+        int max = 0;
+        final int length;
+        if (str == null || (length = str.length()) == 0) {
+            return 0;
+        }
+
+        final Map<Character, Integer> mapping = new HashMap();
+
+        int i = 0, j = 0;
+
+        while (i < length && j < length) {
+            // System.out.println("i:" + i + " j:" + j);
+            // System.out.println("pre：" + mapping);
+            final char charAt = str.charAt(j);
+            if (mapping.containsKey(charAt)) {
+                i = Math.max(i, mapping.get(charAt));
+            }
+
+            max = Math.max(max, j -i +1);
+            mapping.put(charAt, j+1);
+            j++;
+            // System.out.println("after：" + mapping);
+        }
+        return max;
+    }
+    /**
+     * 优化的滑动窗口算法
+     *
+     * @param str
+     * @return
+     */
+    public static Integer lengthOfLongestSubstringSmoothWinOpt0(String str) {
         int max = 0;
         final int length;
         if (str == null || (length=str.length()) == 0) {
             return 0;
+        }
+
+        final Map<Character, Integer> mapping = new HashMap();
+
+        int i = 0, j = 0;
+
+        while (i < length && j < length) {
+            // System.out.println("i:" + i + " j:" + j);
+            // System.out.println("pre："+ mapping);
+            final char charAt = str.charAt(j);
+            if (mapping.containsKey(charAt)) {
+                max = Math.max(max, mapping.size());
+                mapping.remove(str.charAt(i));
+
+                i = Math.max(++i, mapping.getOrDefault(charAt, 0));
+            } else {
+                mapping.put(charAt, j++);
+            }
+            // System.out.println("after："+ mapping);
         }
 
 
@@ -81,16 +188,15 @@ public class Solution003 {
         int i = 0, j = 0;
         final HashSet<Character> container = new HashSet();
         while (i < length && j< length) {
-            final char charAt = str.charAt(j++);
-            if (container.contains(charAt)) {
+            if (container.contains(str.charAt(j))) {
                 container.remove(str.charAt(i++));
             } else {
-                container.add(charAt);
-                max = Math.max(max, j-i );
-                // max = Math.max(max, container.size());
+                container.add(str.charAt(j++));
+                // max = Math.max(max, j-i );
+                max = Math.max(max, container.size());
             }
 
-            System.out.println(container);
+            // System.out.println(container);
 
         }
 
--- src/cn/supertao/utils/MapUtils.java
+++ src/cn/supertao/utils/MapUtils.java
@@ -1,7 +1,6 @@
 package cn.supertao.utils;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author shidt
